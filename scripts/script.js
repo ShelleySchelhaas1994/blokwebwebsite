a2338a7ae2d0b2dca47d9f9031c8647ebb0f99af
ShelleySchelhaas1994 <70640089+ShelleySchelhaas1994@users.noreply.github.com>
2022-09-21 16:09:43 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,6 @@ var deButton = document.querySelector("header section:nth-of-type(1) button");
 deButton.addEventListener('click', openMenu)
 
 function openMenu() {
-var openNav = document.querySelector("header section:nth-of-type(1) nav:nth-of-type(1)")
-openNav.classList.toggle("menuGeopend")
+	var deNav = document.querySelector("header > nav")
+	deNav.classList.toggle("menuGeopend")
 }
